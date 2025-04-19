812b0e84ea8c41cb433fe25506683366a2139dc8
MT <dev.tahirulbari2002@hotmail.com>
2025-04-19 16:31:55 +0500

    setup vercel for deployment

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+//@ts-ignore
+import eslint from "vite-plugin-eslint";
 import tailwindcss from "@tailwindcss/vite";
-import path from 'path'
+import path from "path";
 // https://vite.dev/config/
 
 export default defineConfig({
-    plugins: [react(), eslint(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "src")
-        }
-    }
-})
+  plugins: [react(), eslint(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
